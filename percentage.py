10f93a901a418d1c83c73cc749f3f2908767180b
mrinnocent007 <141913310+mrinnocent007@users.noreply.github.com>
2023-08-10 02:28:31 -0700

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,7 +1,7 @@
 sub1=int(intput("Entere the subject 1 marks")
 sub2=int(input("enter the subject 2 marks")
-sub3=int(input("Enter the 3 marks")
-sub4=int(input("Enter the subjec 4 markss")
+sub3=int(input("Enter the subject 3 marks")
+sub4=int(input("Enter the subject 4 marks")
 
 per=sub1+sub2+sub3+sub4 / 400 * 100
 
