a9cd667761f4ee2128e147219f0f8ea108eb5eba
mrinnocent007 <callmeinnocent007@gmail.com>
2023-08-10 02:27:23 -0700

    python programe

---
+++ percentage.py
@@ -0,0 +1,9 @@
+sub1=int(intput("Entere the subject 1 marks")
+sub2=int(input("enter the subject 2 marks")
+sub3=int(input("Enter the 3 marks")
+sub4=int(input("Enter the subjec 4 markss")
+
+per=sub1+sub2+sub3+sub4 / 400 * 100
+
+print("Percentage of the student is =" ,per)
+
