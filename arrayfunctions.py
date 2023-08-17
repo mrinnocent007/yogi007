7fb9f3919c8a0ff68205582475589d29f8023c6c
darshan <darshandachuu494@gmail.com>
2023-08-17 01:55:16 -0700

    change functios

---
+++ arrayfunctions.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array((1,2,3))
+print("dimension =",arr.ndim)
+print("Shape of array ",arr.shape)
+print("Data  type of an array ",arr.dtype)
+print("size of an array ",arr.size)
+
--- percentage.py
+++
@@ -1,9 +0,0 @@
-sub1=int(intput("Entere the subject 1 marks")
-sub2=int(input("enter the subject 2 marks")
-sub3=int(input("Enter the subject 3 marks")
-sub4=int(input("Enter the subject 4 marks")
-
-per=sub1+sub2+sub3+sub4 / 400 * 100
-
-print("Percentage of the student is =" ,per)
-
