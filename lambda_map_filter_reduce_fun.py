896dcecb82911a389a9318e770a8c295448a7d08
darshan <darshandachuu494@gmail.com>
2023-08-17 23:23:02 -0700

    change function

---
+++ lambda_map_filter_reduce_fun.py
@@ -0,0 +1,20 @@
+#lambda function
+a=lambda x : x+2
+print(a(2))
+
+#map function
+lst =[1,2,3,4,5]
+l=list(map(lambda x : x+5 , lst))
+print(l)
+
+#filter function
+lst =[1,2,3,4,5,6,7,8]
+l=list(filter(lambda x :x%2 ,lst))
+print(l)
+
+#reduce function
+from functools import reduce 
+lst =[1,2,3,4,5,6,7,8]
+y=reduce(lambda x,y : x+y , lst )
+print(y)
+
